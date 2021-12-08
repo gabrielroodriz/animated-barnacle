2f8d41d9e77146343966252a03363ef508fe6e78
Gabriel Rodrigues <33073778+gabrielroodriz@users.noreply.github.com>
2021-12-08 11:01:26 -0300

    feat:Integrate with Chakra UI

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "7b76b64308394089"; }
+/******/ 		__webpack_require__.h = function() { return "f9f93307286300a7"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,19 @@
+import { Container, Box, Heading } from '@chakra-ui/react';
 const Page = () => {
-	return <div>Hello</div>;
+	return (
+		<Container>
+			<Box borderRadius='lg' bg='red' p={3} align='center'>
+				Hello, I&apos;m a Front-End Developer based in São Paulo!
+			</Box>
+			<Box display={{ md: 'flex' }}>
+				<Box flexGrow={1}>
+					<Heading as='h2' variant='page-title'>
+						Gabriel Rodrigues
+					</Heading>
+				</Box>
+			</Box>
+		</Container>
+	);
 };
 
 export default Page;
